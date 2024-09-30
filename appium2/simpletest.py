c293a150f2002a7c313aa4cb04859ad9f81f8d2f
HolmesMR-AL <ross@al.co.za>
2024-09-30 15:37:07 +0200

    localhost

---
+++ appium2/simpletest.py
@@ -0,0 +1,20 @@
+import time
+import os
+import base64
+from appium import webdriver
+from appium.options.android import UiAutomator2Options
+from appium.webdriver.common.appiumby import AppiumBy
+
+desired_caps = dict(
+    platformName="Android",
+    deviceName="Android",
+    browserName="chrome",
+    automationName="UiAutomator2",
+)
+
+capabilities_options = UiAutomator2Options().load_capabilities(desired_caps)
+driver = webdriver.Remote("http://localhost:4723", options=capabilities_options)
+# driver.start_recording_screen()
+driver.get("https://google.com")
+driver.implicitly_wait(5)
+driver.find_element(AppiumBy.XPATH, '//*[@name="q"]').send_keys("Hello World")
