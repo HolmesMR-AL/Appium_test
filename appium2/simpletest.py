ea16c795588ff47c15e1be8a48bf9774d27899c8
HolmesMR-AL <ross@al.co.za>
2024-09-30 23:08:01 +0200

    video test

--- appium2/simpletest.py
+++ appium2/simpletest.py
@@ -14,8 +14,18 @@ desired_caps = dict(
 
 capabilities_options = UiAutomator2Options().load_capabilities(desired_caps)
 driver = webdriver.Remote("http://127.0.0.1:4723", options=capabilities_options)
-print("Driver is ready")
-# driver.start_recording_screen()
+driver.start_recording_screen()
+
 driver.get("https://google.com")
 driver.implicitly_wait(5)
+
 driver.find_element(AppiumBy.XPATH, '//*[@name="q"]').send_keys("Hello World")
+
+video_rawdata = driver.stop_recording_screen()
+
+video_name = driver.current_activity + time.strftime("%Y_%m_%d_%H%M%S")
+
+filepath = f"videos/{video_name}.mp4"
+
+with open(filepath, "wb") as vd:
+    vd.write(base64.b64decode(video_rawdata))
