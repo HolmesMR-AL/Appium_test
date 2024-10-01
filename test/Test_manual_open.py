57de542bcbfb57468be8adb89defa656201a0a02
HolmesMR-AL <ross@al.co.za>
2024-10-01 14:12:12 +0200

    allure test

--- test/Test_manual_open.py
+++ test/Test_manual_open.py
@@ -6,26 +6,19 @@ from appium.options.android import UiAutomator2Options
 from appium.webdriver.common.appiumby import AppiumBy
 from selenium.common.exceptions import NoSuchElementException
 
+desired_caps = dict(
+    deviceName="Android", platformName="Android", automationName="UiAutomator2"
+)
 
-@pytest.fixture(scope="module")
-def driver_setup():
-    desired_caps = dict(
-        deviceName="Android", platformName="Android", automationName="UiAutomator2"
-    )
+capabilities_options = UiAutomator2Options().load_capabilities(desired_caps)
+driver = webdriver.Remote("http://127.0.0.1:4723", options=capabilities_options)
+driver.implicitly_wait(5)
 
-    capabilities_options = UiAutomator2Options().load_capabilities(desired_caps)
-    driver = webdriver.Remote("http://127.0.0.1:4723", options=capabilities_options)
-    driver.implicitly_wait(5)
-    time.sleep(2)
-    yield driver   # Provide the WebDriver instance to the test functions
-    driver.quit()  # Teardown: Quit the WebDriver instance
-
-
-def test_open_chrome(driver):
+def test_open_chrome():
     driver.find_element(AppiumBy.ACCESSIBILITY_ID, "Chrome").click()
 
 
-def test_accept_terms(driver):
+def test_accept_terms():
     try:
         terms_accept_button = driver.find_element(
             AppiumBy.ID, "com.android.chrome:id/terms_accept"
@@ -35,7 +28,7 @@ def test_accept_terms(driver):
         pass
 
 
-def test_dismiss_negative_button(driver):
+def test_dismiss_negative_button():
     try:
         negative_button = driver.find_element(
             AppiumBy.ID, "com.android.chrome:id/negative_button"
@@ -45,7 +38,7 @@ def test_dismiss_negative_button(driver):
         pass
 
 
-def test_search_in_chrome(driver):
+def test_search_in_chrome():
     driver.find_element(AppiumBy.ID, "com.android.chrome:id/search_box_text").send_keys(
         "Hello World"
     )
